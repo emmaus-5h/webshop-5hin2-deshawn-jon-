661caf2f64f6bf69882dec1ab8b45363d2131545
Jonp0612 <115073656+Jonp0612@users.noreply.github.com>
2024-01-26 13:10:32 +0000

    goede versie

--- db/create.sql
+++ db/create.sql
@@ -8,7 +8,6 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2)
-  SELECT 
 );
 CREATE TABLE  (
 teams VARCHAR(200),
@@ -32,5 +31,5 @@ insert into products (name, description, code, price) values ('Scarlet and the B
 insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
 insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
-CREATE TABLE appels 
+
 
