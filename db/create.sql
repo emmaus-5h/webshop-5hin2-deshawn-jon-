0e3cfa4d504efd5cec2a03dbd0b95783c10caf02
deshawn112769 <115638222+deshawn112769@users.noreply.github.com>
2024-01-30 08:59:32 +0000

    10 producten in totaal db

--- db/create.sql
+++ db/create.sql
@@ -39,11 +39,13 @@ teams VARCHAR(200),
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Real Madrid Thuisshirt 23/24', 'Thuis Tenue Real Madrid CF 23/24,', '816905633-0', 55);
-insert into products (name, description, code, price) values ('Feyenoord Thuisshirt 23/24', 'Nulla ut erat id mauris vulputate elementum.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Manchester United Thuisshirt 23/24, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Manchester City Thuisshirt 23/24, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Barcelona Thuisshirt 23/24, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('AC Milan Thuisshirt 23/24', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
-
-
+insert into products (name, description, code, price) values ('Real Madrid Thuisshirt 23/24', 'Thuis Tenue Real Madrid CF 23/24', '816905633-0', 55);
+insert into products (name, description, code, price) values ('Feyenoord Thuisshirt 23/24', 'Thuis Tenue Feyenoord 23/24', '077030122-3', 55);
+insert into products (name, description, code, price) values ('Manchester United Thuisshirt 23/24', 'Thuis Tenue Manchester United 23/24', '445924201-X', 55);
+insert into products (name, description, code, price) values ('Manchester City Thuisshirt 23/24', 'Thuis Tenue Manchester City 23/24', '693155505-7', 55);
+insert into products (name, description, code, price) values ('FC Barcelona Thuisshirt 23/24', 'Thuis Tenue FC Barcelona 23/24', '686928463-6', 55);
+insert into products (name, description, code, price) values ('Inter Milan Thuisshirt 23/24', 'Thuis Tenue AC Milan 23/24', '492662523-7', 55);
+insert into products (name, description, code, price) values ('Paris Saint-Germain FC Thuisshirt 23/24', 'Thuis Tenue Paris Saint-Germain FC 23/24', '692662523-7', 55);
+insert into products (name, description, code, price) values ('FC Bayern München Thuisshirt 23/24', 'Thuis Tenue FC Bayern München 23/24', '482662523-7', 55);
+insert into products (name, description, code, price) values ('Borussia Dortmund Thuisshirt 23/24', 'Thuis Tenue Borussia Dortmund 23/24', '452662523-7', 55);
+insert into products (name, description, code, price) values ('Arsenal FC Thuisshirt 23/24', 'Thuis Tenue Arsenal FC 23/24', '492663523-7', 55);
