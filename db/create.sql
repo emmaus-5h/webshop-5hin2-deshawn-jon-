6a767a4d9aaab6490f08caa428945586d6c58a04
Jonp0612 <115073656+Jonp0612@users.noreply.github.com>
2024-02-18 16:24:01 +0000

    API aangepast + laaste nieuwe DB

--- db/create.sql
+++ db/create.sql
@@ -19,6 +19,7 @@
     name VARCHAR(30),
     land_id INTEGER,
     materiaal_id INTEGER,
+    kleur_id INTEGER,
     description TEXT,
     price NUMERIC(10, 2)
   );
@@ -28,8 +29,18 @@
     );
   CREATE TABLE materiaal (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(100)
+    name VARCHAR(255)
     );
+CREATE TABLE kleur (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255)
+  );
+
+CREATE TABLE ProductMetLand (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  products_id INTEGER,
+  land_id INTEGER,
+);
 
 
 
@@ -45,16 +56,16 @@
   -- want different data? check: https://www.mockaroo.com/910b6c20
   --
 
-  insert into products (name, description, code, price, land_id, materiaal_id) values ('Real Madrid Thuisshirt 23/24', 'Thuis Tenue Real Madrid CF 23/24', '816905633-0', 89, 1, 1);
-  insert into products (name, description, code, price, land_id, materiaal_id) values ('Feyenoord Thuisshirt 23/24', 'Thuis Tenue Feyenoord 23/24', '077030122-3', 90, 2, 1);
-  insert into products (name, description, code, price, land_id, materiaal_id) values ('Manchester United Thuisshirt 23/24', 'Thuis Tenue Manchester United 23/24', '445924201-X', 99, 3, 1);
-  insert into products (name, description, code, price, land_id, materiaal_id) values ('Manchester City Thuisshirt 23/24', 'Thuis Tenue Manchester City 23/24', '693155505-7', 89, 3, 1);
-  insert into products (name, description, code, price, land_id, materiaal_id) values ('FC Barcelona Thuisshirt 23/24', 'Thuis Tenue FC Barcelona 23/24', '686928463-6', 99, 1, 1);
-  insert into products (name, description, code, price, land_id, materiaal_id) values ('Inter Milan Thuisshirt 23/24', 'Thuis Tenue AC Milan 23/24', '492662523-7', 80, 4, 1);
-  insert into products (name, description, code, price, land_id, materiaal_id) values ('Paris Saint-Germain FC Thuisshirt 23/24', 'Thuis Tenue Paris Saint-Germain FC 23/24', '692662523-7', 110, 1, 1);
-  insert into products (name, description, code, price, land_id, materiaal_id) values ('FC Bayern München Thuisshirt 23/24', 'Thuis Tenue FC Bayern München 23/24', '482662523-7', 85, 6, 1);
-  insert into products (name, description, code, price, land_id, materiaal_id) values ('Borussia Dortmund Thuisshirt 23/24', 'Thuis Tenue Borussia Dortmund 23/24', '452662523-7', 89, 6, 1);
-  insert into products (name, description, code, price, land_id, materiaal_id) values ('Arsenal FC Thuisshirt 23/24', 'Thuis Tenue Arsenal FC 23/24', '492663523-7', 99, 3, 1);
+  insert into products (name, description, code, price, land_id, materiaal_id, kleur_id) values ('Real Madrid Thuisshirt 23/24', 'Thuis Tenue Real Madrid CF 23/24', '816905633-0', 89, 1, 1, 5);
+  insert into products (name, description, code, price, land_id, materiaal_id, kleur_id) values ('Feyenoord Thuisshirt 23/24', 'Thuis Tenue Feyenoord 23/24', '077030122-3', 90, 2, 1, 4);
+  insert into products (name, description, code, price, land_id, materiaal_id, kleur_id) values ('Manchester United Thuisshirt 23/24', 'Thuis Tenue Manchester United 23/24', '445924201-X', 99, 3, 1, 2);
+  insert into products (name, description, code, price, land_id, materiaal_id, kleur_id) values ('Manchester City Thuisshirt 23/24', 'Thuis Tenue Manchester City 23/24', '693155505-7', 89, 3, 1, 4);
+  insert into products (name, description, code, price, land_id, materiaal_id, kleur_id) values ('FC Barcelona Thuisshirt 23/24', 'Thuis Tenue FC Barcelona 23/24', '686928463-6', 99, 1, 1, 5);
+  insert into products (name, description, code, price, land_id, materiaal_id, kleur_id) values ('Inter Milan Thuisshirt 23/24', 'Thuis Tenue AC Milan 23/24', '492662523-7', 80, 4, 1, 6);
+  insert into products (name, description, code, price, land_id, materiaal_id, kleur_id) values ('Paris Saint-Germain FC Thuisshirt 23/24', 'Thuis Tenue Paris Saint-Germain FC 23/24', '692662523-7', 110, 1, 1, 5);
+  insert into products (name, description, code, price, land_id, materiaal_id, kleur_id) values ('FC Bayern München Thuisshirt 23/24', 'Thuis Tenue FC Bayern München 23/24', '482662523-7', 85, 6, 1, 4);
+  insert into products (name, description, code, price, land_id, materiaal_id, kleur_id) values ('Borussia Dortmund Thuisshirt 23/24', 'Thuis Tenue Borussia Dortmund 23/24', '452662523-7', 89, 6, 1, 7);
+  insert into products (name, description, code, price, land_id, materiaal_id, kleur_id) values ('Arsenal FC Thuisshirt 23/24', 'Thuis Tenue Arsenal FC 23/24', '492663523-7', 99, 3, 1, 8);
 
 
 --landen
@@ -67,3 +78,76 @@ insert into land (name) values ('duitsland');
 
 --materiaal
 insert into materiaal (name) values ('polyester'); 
+
+--kleur
+insert into kleur (name) values ('blauw'); 
+insert into kleur (name) values ('wit'); 
+insert into kleur (name) values ('rood'); 
+insert into kleur (name) values ('geel'); 
+insert into kleur (name) values ('zwart'); 
+insert into kleur (name) values ('lichtblauw'); 
+insert into kleur (name) values ('donkerblauw'); 
+insert into kleur (name) values ('goud'); 
+
+
+-- product en Land
+ insert into ProductMetLand (products_id,land_id) values (1, 1 );
+  insert into ProductMetLand (products_id,land_id) values (1, 3 );
+  insert into ProductMetLand (products_id,land_id) values (1, 4 );
+  insert into ProductMetLand (products_id,land_id) values (2, 1);
+  insert into ProductMetLand (products_id,land_id) values (2, 6);
+  insert into ProductMetLand (products_id,land_id) values (2, 6);
+  insert into ProductMetLand (products_id,land_id) values (2, 5);
+  insert into ProductMetLand (products_id,land_id) values (3, 4);
+  insert into ProductMetLand (products_id,land_id) values (3, 2);
+  insert into ProductMetLand (products_id,land_id) values (3, 4);
+  insert into ProductMetLand (products_id,land_id) values (3, 3);
+  insert into ProductMetLand (products_id,land_id) values (3, 5);
+  insert into ProductMetLand (products_id,land_id) values (3, 3);
+  insert into ProductMetLand (products_id,land_id) values (4, 6);
+  insert into ProductMetLand (products_id,land_id) values (4, 5);
+  insert into ProductMetLand (products_id,land_id) values (4, 2);
+  insert into ProductMetLand (products_id,land_id) values (4, 5);
+  insert into ProductMetLand (products_id,land_id) values (4, 5);
+  insert into ProductMetLand (products_id,land_id) values (4, 1);
+  insert into ProductMetLand (products_id,land_id) values (5, 1);
+  insert into ProductMetLand (products_id,land_id) values (5, 3);
+  insert into ProductMetLand (products_id,land_id) values (5, 2);
+  insert into ProductMetLand (products_id,land_id) values (5, 3);
+  insert into ProductMetLand (products_id,land_id) values (5, 2);
+  insert into ProductMetLand (products_id,land_id) values (5, 4);
+  insert into ProductMetLand (products_id,land_id) values (6, 1);
+  insert into ProductMetLand (products_id,land_id) values (6, 1);
+  insert into ProductMetLand (products_id,land_id) values (6, 7);
+  insert into ProductMetLand (products_id,land_id) values (6, 5);
+  insert into ProductMetLand (products_id,land_id) values (6, 3);
+  insert into ProductMetLand (products_id,land_id) values (7, 8);
+  insert into ProductMetLand (products_id,land_id) values (7, 4);
+  insert into ProductMetLand (products_id,land_id) values (7, 7);
+  insert into ProductMetLand (products_id,land_id) values (7, 6);
+  insert into ProductMetLand (products_id,land_id) values (7, 5);
+  insert into ProductMetLand (products_id,land_id) values (7, 4);
+  insert into ProductMetLand (products_id,land_id) values (8, 1);
+  insert into ProductMetLand (products_id,land_id) values (8, 4);
+  insert into ProductMetLand (products_id,land_id) values (8, 2);
+  insert into ProductMetLand (products_id,land_id) values (8, 5);
+  insert into ProductMetLand (products_id,land_id) values (8, 6);
+  insert into ProductMetLand (products_id,land_id) values (8, 2);
+  insert into ProductMetLand (products_id,land_id) values (8, 1);
+  insert into ProductMetLand (products_id,land_id) values (9, 1);
+  insert into ProductMetLand (products_id,land_id) values (9, 6);
+  insert into ProductMetLand (products_id,land_id) values (9, 5);
+  insert into ProductMetLand (products_id,land_id) values (9, 3);
+  insert into ProductMetLand (products_id,land_id) values (9, 4);
+  insert into ProductMetLand (products_id,land_id) values (9, 4);
+  insert into ProductMetLand (products_id,land_id) values (10, 6);
+  insert into ProductMetLand (products_id,land_id) values (10, 1);
+  insert into ProductMetLand (products_id,land_id) values (10, 5);
+  insert into ProductMetLand (products_id,land_id) values (10, 5);
+  insert into ProductMetLand (products_id,land_id) values (10, 3);
+  insert into ProductMetLand (products_id,land_id) values (10, 4);
+
+
+
+
+
